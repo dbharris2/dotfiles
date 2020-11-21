c0b717946b4bc679a8586d744c4f8635231b0e96
Devon Harris <dbharris2@gmail.com>
2020-11-20 19:03:21 -0800

    [fish] Reorganize fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,31 +12,20 @@ set fish_greeting
 
 ### ALIASES ###
 
-# Destructive Actions
-alias cp='cp -i'
-alias mv='mv -i'
-alias rm='rm -i'
-
-# Git (Dotfiles)
-alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
-
-# Image Viewing
-alias preview='feh --scale-down --auto-zoom'
-
-# ls
-alias ls='exa -abhl --group-directories-first'
-
-# Navigation
 alias ..='cd ..'
-alias .2='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-
-# Pacman
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
 alias cleanup='sudo pacman -Qtdq | sudo pacman -Rns -' # Remove orphaned packages
-
-# Text Editing
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+alias cp='cp -i'
+alias ls='exa -abhl --group-directories-first'
+alias mv='mv -i'
+alias preview='feh --scale-down --auto-zoom'
+alias rm='rm -i'
 alias vim='nvim'
+
+### FUNCTIONS ###
 
 function fish_mode_prompt
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
@@ -65,7 +54,6 @@ function fish_mode_prompt
 end
 
 function fish_user_key_bindings
-    # fish_default_key_bindings
     fish_vi_key_bindings
 end
 
